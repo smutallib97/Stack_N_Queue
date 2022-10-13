f1a584b90a598107e1bfb403a545e8c0d1bfc985
smutallib97 <smutallib97@gmail.com>
2022-10-13 17:02:10 +0530

    Ability to create a queue

---
+++ Stack_n_Queue2.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class Stack_n_Queue2 {
+    LinkedList<Integer> newlist = new LinkedList<Integer>();
+
+    public void enQueue(Integer Data) {
+        newlist.addLast(Data);
+    }
+
+    public void PrintQueue() {
+        System.out.println(newlist);
+    }
+
+    public static void main(String[] args) {
+        //UC1 - Ability to create a Queue
+        Stack_n_Queue2 myQueue = new Stack_n_Queue2();
+
+        myQueue.enQueue(56);
+        myQueue.enQueue(30);
+        myQueue.enQueue(70);
+
+        myQueue.PrintQueue();
+    }
+}
