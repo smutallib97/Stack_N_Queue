b5fb384f32f88a2f989fcbb5a649df09ac25204b
smutallib97 <smutallib97@gmail.com>
2022-10-13 16:19:05 +0530

    Welcome to Stack N Queue Program

---
+++ Stack_n_Queue.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Stack_n_Queue {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack And Queue Program by Using LinkedList Operations");
+    }
+}
