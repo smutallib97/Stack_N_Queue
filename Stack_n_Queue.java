a2c218f99b8ca49129881fa0341bacc2a6fbe9a6
smutallib97 <smutallib97@gmail.com>
2022-10-13 16:32:19 +0530

    Ability to create a Stack

--- Stack_n_Queue.java
+++ Stack_n_Queue.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
+import java.util.LinkedList;
+
 public class Stack_n_Queue {
+    static class MyStack {
+        LinkedList<Integer> newlist =new LinkedList<Integer>();
+        public void Push(Integer Data) {
+            newlist.add(Data);
+        }
+        public void PrintStack() {
+            System.out.println(newlist);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Stack And Queue Program by Using LinkedList Operations");
+        MyStack mystack = new MyStack();
+        System.out.println("Elements of Stack are : ");
+        mystack.Push(56);
+        mystack.Push(30);
+        mystack.Push(70);
+
+        mystack.PrintStack();
     }
 }
